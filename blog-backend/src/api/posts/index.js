ebe5ecfa45779191b8b4544ffdcd8678a8b14434
jazz_iron <dlwocjf90@gmail.com>
2020-12-06 13:26:57 +0900

    23장 JWT 회원인증
    23장 JWT 회원인증

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -1,17 +1,24 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIn from '../../lib/checkLoggedIn';
+
+const winston = require('winston');
+const logger = winston.createLogger();
+
 
 
 const posts = new Router();
 
 posts.get('/' , postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIn, postsCtrl.write);
 
 const post = new Router();
 post.get('/:id', postsCtrl.read);
-post.delete('/:id', postsCtrl.remove);
-post.patch('/:id', postsCtrl.update);
+post.delete('/:id', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/:id', checkLoggedIn, postsCtrl.checkOwnPost, postsCtrl.update);
 
-posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+posts.use('/:id', postsCtrl.getPostById, post.routes());
+
+logger.info('TEST'+ posts );
 
 export default posts;
--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -3,16 +3,39 @@ import Post from '../../models/post';
 import mongoose from 'mongoose';
 import Joi from 'joi';
 
+const winston = require('winston');
+const logger = winston.createLogger();
+
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
     const { id } = ctx.params;
     if ( !ObjectId.isValid(id)) {
         ctx.status = 400;
         return;
     }
-    return next;
-}
+    try {
+        const post = await Post.findById(id);
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.state.post = post;
+        return next();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+/**로그인 중인 사용자 확인 */
+export const checkOwnPost = (ctx, next) => {
+    const { user, post } = ctx.state;
+    if (post.user._id.toString() !== user._id) {
+        ctx.status = 403;
+        return;
+    }
+    return next();
+};
 
 /*
     POST /api/posts
@@ -45,6 +68,7 @@ export const write = async ctx => {
         title,
         body,
         tags,
+        user: ctx.state.user,
     });
     try {
         await post.save();
@@ -68,14 +92,21 @@ export const list = async ctx => {
         return;
     }
 
+    //tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음
+    const { tag, username } = ctx.query;
+    const query = {
+        ...(username ? { 'user.username' : username} : {}),
+        ...(tag ? { tags : tag } : {}),
+    };
+
     try {
-        const posts = await Post.find()
+        const posts = await Post.find(query)
         .sort({ _id: -1 })
         .limit(10)
         .skip((page - 1) * 10)
         .lean() //처음부터 JSON 형태로 데이터 조회 ( = .map(post => post.toJSON()))
         .exec();
-        const postCount = await Post.countDocuments().exec();
+        const postCount = await Post.countDocuments(query).exec();
         ctx.set('Last-Page', Math.ceil(postCount / 10));
         ctx.body = posts
             .map(post => ({
@@ -92,17 +123,7 @@ export const list = async ctx => {
     GET /api/posts/:id
  */
 export const read = async ctx => {
-    const { id } = ctx.params;
-    try {
-        const post = await Post.findById(id).exec();
-        if (!post) {
-            ctx.status = 404;
-            return;
-        }
-        ctx.body = post;
-    } catch (e) {
-        ctx.throw(500, e);
-    }
+    ctx.body = ctx.state.post;
 };
 
 /**
@@ -110,6 +131,7 @@ export const read = async ctx => {
  */
  export const remove = async ctx => {
      const { id } = ctx.params;
+     logger.info('you come in path to '/'id' );
      try {
          await Post.findByIdAndRemove(id).exec();
          ctx.status = 204;  // No Content 성공하기는 했지만 응답할 데이터는 없음
