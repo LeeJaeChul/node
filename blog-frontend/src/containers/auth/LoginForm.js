483c19773f04a4ab4002d7b59a65d1a70258e7a3
jazz_iron <dlwocjf90@gmail.com>
2020-12-13 17:58:43 +0900

    part 24
    part24 로그인 로그아웃 회원가입 기능 구현

--- blog-frontend/src/containers/auth/LoginForm.js
+++ blog-frontend/src/containers/auth/LoginForm.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeField, initializeForm } from '../../modules/auth';
+import { withRouter } from 'react-router-dom';
+import { changeField, initializeForm, login } from '../../modules/auth';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
 
-
-const LoginForm = () => {
+const LoginForm = ({ history }) => {
+    const [error, setError] = useState(null);
     const dispatch = useDispatch();
-    const { form } = useSelector(({ auth }) => ({
-        form: auth.login
-    })); //상태가 변경되지 않으면 리렌덩링 x 
+
+    const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
+        form: auth.login,
+        auth: auth.auth,
+        authError: auth.authError,
+        user: user.user
+    })); //상태가 변경되지 않으면 리렌더링 x 
 
     //인풋 변경 이벤트 핸들러
     const onChange = e => {
@@ -24,6 +30,8 @@ const LoginForm = () => {
 
     const onSubmit = e => {
         e.preventDefault();
+        const { username, password } = form;
+        dispatch(login({ username, password}));
         //구현 예정
     };
 
@@ -32,14 +40,37 @@ const LoginForm = () => {
         dispatch(initializeForm('login'));
     }, [dispatch]);
 
+    useEffect(() => {
+        if (authError) {
+            setError('로그인 실패');
+            return;
+        }
+        if (auth) {
+            console.log('로그인 성공');
+            dispatch(check());
+        }
+    }, [auth, authError, dispatch]);
+
+    useEffect(() => {
+        if (user) {
+            history.push('/');
+            try { 
+                localStorage.setItem('user', JSON.stringify(user));
+            } catch (e) {
+                console.log('localStorage is not working');
+            }
+        }
+    }, [history, user]);
+
     return (
         <AuthForm
             type="login"
             form={form}
             onChange={onChange}
             onSubmit={onSubmit}
+            error={error}
         />
     );
 };
 
-export default LoginForm;
+export default withRouter(LoginForm);
